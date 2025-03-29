cf1df80b8eba73f22260f2694be3186726241766
Irinel <andreimc49@gmail.com>
2025-03-29 14:52:24 +0200

    created tests for OrderController and ProductCategoryController

---
+++ backend/ResourceServer/ResourcesServes.Tests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using ResourceServer.Controllers;
+using ResourceServer.Data.Models;
+using ResourceServer.Data.Repositories;
+
+namespace ResourcesServes.Tests.Controllers;
+
+[TestClass]
+public class OrderControllerTests
+{
+    private IOrderRepository _repository;
+    private OrderController _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repository = Substitute.For<IOrderRepository>();
+        _controller = new OrderController(_repository);
+    }
+
+    [TestMethod]
+    public void GetAll_ShouldReturnOkResult_WhenRepositoryReturnsListOfOrders()
+    {
+        var orders = GetOrders();
+        _repository.GetAll().Returns(orders);
+
+        var actionResult = _controller.GetAll();
+
+        var okResult = actionResult.Result as OkObjectResult;
+        Assert.IsNotNull(okResult, "Expected an OkObjectResult");
+        var returnedOrders = okResult.Value as IEnumerable<Order>;
+        Assert.IsNotNull(returnedOrders, "Expected a list of Order");
+        CollectionAssert.AreEqual(new List<Order>(orders), new List<Order>(returnedOrders));
+    }
+
+    [TestMethod]
+    public void GetById_ShouldReturnOkResult_WhenOrderExists()
+    {
+        var order = GetOrder();
+        _repository.GetById(1).Returns(order);
+
+        var actionResult = _controller.GetById(1);
+
+        var okResult = actionResult.Result as OkObjectResult;
+        Assert.IsNotNull(okResult, "Expected an OkObjectResult");
+        var returnedOrder = okResult.Value as Order;
+        Assert.IsNotNull(returnedOrder, "Expected an Order");
+        Assert.AreEqual(order.Id, returnedOrder.Id, "Order Id should match");
+    }
+
+    [TestMethod]
+    public void GetById_ShouldReturnNotFound_WhenOrderDoesNotExist()
+    {
+        _repository.GetById(1).Returns((Order)null);
+
+        var actionResult = _controller.GetById(1);
+
+        Assert.IsInstanceOfType<NotFoundResult>(actionResult.Result, "Expected a NotFoundResult");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnCreatedAtActionResult_WhenOrderCreated()
+    {
+        var newOrder = GetOrder();
+        var createdOrder = GetOrder();
+        _repository.Add(newOrder).Returns(createdOrder);
+
+        var actionResult = _controller.Create(newOrder);
+
+        var createdAtActionResult = actionResult.Result as CreatedAtActionResult;
+        Assert.IsNotNull(createdAtActionResult, "Expected a CreatedAtActionResult");
+        Assert.AreEqual(nameof(_controller.GetById), createdAtActionResult.ActionName, "Expected action name to be GetById");
+        var returnedOrder = createdAtActionResult.Value as Order;
+        Assert.IsNotNull(returnedOrder, "Expected an Order");
+        Assert.AreEqual(createdOrder.Id, returnedOrder.Id, "Created order Id should match");
+    }
+
+    [TestMethod]
+    public void Update_ShouldReturnNoContent_WhenOrderExists()
+    {
+        var orderToUpdate = GetOrder();
+        _repository.Update(1, orderToUpdate).Returns(true);
+
+        var result = _controller.Update(1, orderToUpdate);
+
+        Assert.IsInstanceOfType<NoContentResult>(result, "Expected a NoContentResult");
+    }
+
+    [TestMethod]
+    public void Update_ShouldReturnNotFound_WhenOrderDoesNotExist()
+    {
+        var orderToUpdate = GetOrder();
+        _repository.Update(1, orderToUpdate).Returns(false);
+
+        var result = _controller.Update(1, orderToUpdate);
+
+        Assert.IsInstanceOfType<NotFoundResult>(result, "Expected a NotFoundResult");
+    }
+
+    [TestMethod]
+    public void Delete_ShouldReturnNoContent_WhenOrderExists()
+    {
+        _repository.Delete(1).Returns(true);
+
+        var result = _controller.Delete(1);
+
+        Assert.IsInstanceOfType<NoContentResult>(result, "Expected a NoContentResult");
+    }
+
+    [TestMethod]
+    public void Delete_ShouldReturnNotFound_WhenOrderDoesNotExist()
+    {
+        _repository.Delete(1).Returns(false);
+
+        var result = _controller.Delete(1);
+
+        Assert.IsInstanceOfType<NotFoundResult>(result, "Expected a NotFoundResult");
+    }
+
+    private static Order GetOrder() => GetOrders().First();
+
+    private static List<Order> GetOrders() =>
+        [
+        new Order
+        {
+            Id = 1,
+            Status = new OrderStatus
+            {
+                Id = 1,
+                Name = "Pending"
+            },
+            AccountId = "acc1",
+            Address = "123 Main St",
+            OrderTime = DateTime.UtcNow
+        },
+        new Order
+        {
+            Id = 2,
+            Status = new OrderStatus
+            {
+                Id = 1,
+                Name = "Pending"
+            },
+            AccountId = "acc2",
+            Address = "456 Elm St",
+            OrderTime = DateTime.UtcNow
+        }
+    ];
+}
---
+++ backend/ResourceServer/ResourcesServes.Tests/Controllers/ProductCategoryControllerTests.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using ResourceServer.Controllers;
+using ResourceServer.Data.Models;
+using ResourceServer.Data.Repositories;
+
+namespace ResourcesServes.Tests.Controllers;
+
+[TestClass]
+public class ProductCategoryControllerTests
+{
+    private IProductCategoryRepository _repository;
+    private ProductCategoryController _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repository = Substitute.For<IProductCategoryRepository>();
+        _controller = new ProductCategoryController(_repository);
+    }
+
+    [TestMethod]
+    public void GetAll_ShouldReturnsOkResult_WhenRepositoryRetunrListOfCategories()
+    {
+        var categories = new List<ProductCategory>
+        {
+            new() { Id = 1, Name = "Category1" },
+            new() { Id = 2, Name = "Category2" }
+        };
+        _repository.GetAll().Returns(categories);
+
+        var actionResult = _controller.GetAll();
+
+        var okResult = actionResult.Result as OkObjectResult;
+        Assert.IsNotNull(okResult, "Expected an OkObjectResult");
+        var returnedCategories = okResult.Value as IEnumerable<ProductCategory>;
+        Assert.IsNotNull(returnedCategories, "Expected a list of ProductCategory");
+        CollectionAssert.AreEqual(new List<ProductCategory>(categories), new List<ProductCategory>(returnedCategories));
+    }
+
+    [TestMethod]
+    public void GetById_ShouldReturnOkResult_WhenCategoryExists()
+    {
+        var category = new ProductCategory { Id = 1, Name = "Category1" };
+        _repository.GetById(1).Returns(category);
+
+        var actionResult = _controller.GetById(1);
+
+        var okResult = actionResult.Result as OkObjectResult;
+        Assert.IsNotNull(okResult, "Expected an OkObjectResult");
+        var returnedCategory = okResult.Value as ProductCategory;
+        Assert.IsNotNull(returnedCategory, "Expected a ProductCategory");
+        Assert.AreEqual(category.Id, returnedCategory.Id, "Category Id should match");
+    }
+
+    [TestMethod]
+    public void GetById_ShouldReturnNotFound_WhenCategoryDoesNotExist()
+    {
+        _repository.GetById(1).Returns((ProductCategory)null);
+
+        var actionResult = _controller.GetById(1);
+
+        Assert.IsInstanceOfType<NotFoundResult>(actionResult.Result, "Expected a NotFoundResult");
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnCreatedAtActionResult_WhenCategoryCreated()
+    {
+        var newCategory = new ProductCategory { Name = "New Category" };
+        var createdCategory = new ProductCategory { Id = 1, Name = "New Category" };
+        _repository.Add(newCategory).Returns(createdCategory);
+
+        var actionResult = _controller.Create(newCategory);
+
+        var createdAtResult = actionResult.Result as CreatedAtActionResult;
+        Assert.IsNotNull(createdAtResult, "Expected a CreatedAtActionResult");
+        Assert.AreEqual(nameof(_controller.GetById), createdAtResult.ActionName, "Expected action name to be GetById");
+        var returnedCategory = createdAtResult.Value as ProductCategory;
+        Assert.IsNotNull(returnedCategory, "Expected a ProductCategory");
+        Assert.AreEqual(createdCategory.Id, returnedCategory.Id, "Created category Id should match");
+    }
+
+    [TestMethod]
+    public void Update_ShouldReturnNoContent_WhenCategoryExists()
+    {
+        var categoryToUpdate = new ProductCategory { Id = 1, Name = "Updated Category" };
+        _repository.Update(1, categoryToUpdate).Returns(true);
+
+        var result = _controller.Update(1, categoryToUpdate);
+
+        Assert.IsInstanceOfType<NoContentResult>(result, "Expected a NoContentResult");
+    }
+
+    [TestMethod]
+    public void Update_ShouldReturnNotFound_WhenCategoryDoesNotExist()
+    {
+        var categoryToUpdate = new ProductCategory { Id = 1, Name = "Updated Category" };
+        _repository.Update(1, categoryToUpdate).Returns(false);
+
+        var result = _controller.Update(1, categoryToUpdate);
+
+        Assert.IsInstanceOfType<NotFoundResult>(result, "Expected a NotFoundResult");
+    }
+
+    [TestMethod]
+    public void Delete_ShouldReturnNoContent_WhenCategoryExists()
+    {
+        _repository.Delete(1).Returns(true);
+
+        var result = _controller.Delete(1);
+
+        Assert.IsInstanceOfType<NoContentResult>(result, "Expected a NoContentResult");
+    }
+
+    [TestMethod]
+    public void Delete_ShouldReturnNotFound_WhenCategoryDoesNotExist()
+    {
+        _repository.Delete(1).Returns(false);
+
+        var result = _controller.Delete(1);
+
+        Assert.IsInstanceOfType<NotFoundResult>(result, "Expected a NotFoundResult");
+    }
+}
