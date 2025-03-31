076cb3b0fc193dcabb71214dc43d3e86faae17a4
macovei-alexandru <maccelmare@gmail.com>
2025-03-31 23:10:43 +0300

    JIRA-13: D from CRUD on products

--- frontend/MicroBiteClient/src/admin/products/components/DeleteProductDialog.tsx
+++ frontend/MicroBiteClient/src/admin/products/components/DeleteProductDialog.tsx
@@ -1,17 +1,45 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import Button from "../../../components/Button";
 import { BaseDialogProps } from "../types/BaseDialogProps";
+import { useProductsQuery } from "../../../api/hooks/useProductsQuery";
+import { useQueryClient } from "@tanstack/react-query";
+import { Product } from "../../../api/types/Product";
+import ContainedImage from "../../../components/ContainedImage";
+import ErrorLabel from "../../../components/ErrorLabel";
+import { resApi } from "../../../api";
 
 type DeleteProductDialogProps = BaseDialogProps;
 
 export default function DeleteProductDialog({ isVisible, closeDialog }: DeleteProductDialogProps) {
-  const saveChanges = useCallback(async () => {
+  const queryClient = useQueryClient();
+  const productsQuery = useProductsQuery();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const handleProductChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedProduct = productsQuery.data?.find((p) => p.id === Number(e.target.value));
+      if (selectedProduct) {
+        setProduct(selectedProduct);
+      }
+    },
+    [productsQuery.data]
+  );
+
+  const confirmDelete = useCallback(async () => {
     try {
+      setIsSaving(true);
+      setError(null);
+      await resApi.delete(`/Product/${product?.id}`);
+      queryClient.invalidateQueries({ queryKey: ["products"] });
       closeDialog();
     } catch (error) {
       console.error("Error deleting product:", error);
+    } finally {
+      setIsSaving(false);
     }
-  }, [closeDialog]);
+  }, [closeDialog, queryClient, product]);
 
   if (!isVisible) {
     return null;
@@ -20,15 +48,38 @@ export default function DeleteProductDialog({ isVisible, closeDialog }: DeletePr
   return (
     <div
       onClick={closeDialog}
-      className="absolute inset-0 flex justify-center items-center backdrop-blur-md"
+      className="absolute inset-0 flex flex-col justify-center items-center backdrop-blur-md"
     >
       <div
-        className="bg-white p-6 rounded-lg shadow-lg w-96 relative"
+        className="bg-white p-12 rounded-lg shadow-lg min-w-96 w-auto relative flex flex-col gap-4 items-center"
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-xl font-bold mb-4 text-gray-800">Delete Product</h2>
-        <p className="text-gray-600 mb-8">Dialog content goes here...</p>
-        <Button text="Save changes" onClick={saveChanges} />
+        <ErrorLabel error={error} />
+        <div>
+          <select
+            name="product"
+            disabled={productsQuery.isLoading}
+            onChange={handleProductChange}
+            className="w-full appearance-none p-2 border border-gray-300 rounded-lg outline-none focus:ring-3 focus:ring-blue-500 transition duration-500 text-blue-500 font-bold"
+          >
+            {productsQuery.data?.map((product) => (
+              <option key={product.id} value={product.id} className="font-bold">
+                {product.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="w-120 h-80 flex flex-col justify-center items-center">
+          {product?.image && <ContainedImage image={product.image} />}
+        </div>
+
+        <Button
+          text="Confirm delete"
+          disabled={productsQuery.isLoading || isSaving}
+          onClick={confirmDelete}
+        />
       </div>
     </div>
   );
--- frontend/MicroBiteClient/src/menu/components/ProductCard.tsx
+++ frontend/MicroBiteClient/src/menu/components/ProductCard.tsx
@@ -1,4 +1,3 @@
-import Button from "../../components/Button";
 import { Product } from "../../api/types/Product";
 import { defaultProductImage } from "../../assets/defaultProductImage";
 
@@ -11,12 +10,13 @@ export default function ProductCard({ product, onAddToCart }: ProductCardProps) 
   return (
     <div
       key={product.id}
-      className="p-4 border border-blue-500 rounded-lg shadow-md flex flex-col h-full"
+      onClick={() => onAddToCart(product)}
+      className="p-4 border border-blue-500 hover:bg-blue-100 transition duration-500 rounded-lg shadow-md flex flex-col cursor-pointer"
     >
       <img
         src={product.image || defaultProductImage}
         alt={product.name}
-        className="w-full h-40 object-cover rounded-lg mb-2"
+        className="w-full h-40 object-contain rounded-lg mb-2"
       />
       <div className="flex flex-col flex-1">
         <h2 className="text-lg font-semibold text-gray-900">{product.name}</h2>
@@ -25,9 +25,6 @@ export default function ProductCard({ product, onAddToCart }: ProductCardProps) 
         </p>
         <div className="flex flex-row justify-between items-center mt-auto">
           <p className="text-lg font-bold text-blue-500">${product.price.toFixed(2)}</p>
-          <div>
-            <Button text="Add to Cart" onClick={() => onAddToCart(product)} />
-          </div>
         </div>
       </div>
     </div>
