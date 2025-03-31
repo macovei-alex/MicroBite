88b15f31f52889bca4c8e46ef98f9ed60cb7838b
macovei-alexandru <maccelmare@gmail.com>
2025-03-31 23:30:26 +0300

    JIRA-13: better menu UI

--- frontend/MicroBiteClient/src/menu/components/ProductCard.tsx
+++ frontend/MicroBiteClient/src/menu/components/ProductCard.tsx
@@ -13,11 +13,13 @@ export default function ProductCard({ product, onAddToCart }: ProductCardProps) 
       onClick={() => onAddToCart(product)}
       className="p-4 border border-blue-500 hover:bg-blue-100 transition duration-500 rounded-lg shadow-md flex flex-col cursor-pointer"
     >
-      <img
-        src={product.image || defaultProductImage}
-        alt={product.name}
-        className="w-full h-40 object-contain rounded-lg mb-2"
-      />
+      <div className="flex flex-col justify-center items-center">
+        <img
+          src={product.image || defaultProductImage}
+          alt={product.name}
+          className="w-full h-40 object-contain rounded-lg mb-2"
+        />
+      </div>
       <div className="flex flex-col flex-1">
         <h2 className="text-lg font-semibold text-gray-900">{product.name}</h2>
         <p className="text-sm text-gray-600 flex-1 line-clamp-3 mb-4 min-h-[5em]">
