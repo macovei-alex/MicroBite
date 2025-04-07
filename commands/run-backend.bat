b599b06b7fc7aa94d5d963e7640f8d2128c0414b
macovei-alexandru <maccelmare@gmail.com>
2025-04-08 00:44:54 +0300

    JIRA-53: fixed run-backend script

--- commands/run-backend.bat
+++ commands/run-backend.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set basedir=..\backend
+set "basedir=%~dp0..\backend"
 wt -w 0 new-tab --title "Auth Server" cmd /k "cd %basedir%\AuthServer\AuthServer && dotnet run"
 wt -w 0 new-tab --title "Resource Server" cmd /k "cd %basedir%\ResourceServer\ResourceServer && dotnet run"
 endlocal
