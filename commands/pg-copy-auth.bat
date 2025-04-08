4f423554bc78bbe8232fc5dbd677222adf6ffaa3
macovei-alexandru <maccelmare@gmail.com>
2025-04-08 16:24:27 +0300

    JIRA-53: added dynamically changing of file terminations from default crlf to lf for bash scripts

--- commands/pg-copy-auth.bat
+++ commands/pg-copy-auth.bat
@@ -3,5 +3,5 @@ setlocal
 set "basedir=%~dp0.."
 set PGPASSWORD=1q2w3e
 pg_dump -U postgres -h localhost -d MicroBiteAuth -F c -b -v -f "%basedir%/data/pg-auth-data/app/microbiteauth.dump"
-docker exec -it microbite-auth-db-1 bash -c "chmod +x pg-restore-auth.sh && ./pg-restore-auth.sh"
+docker exec -it microbite-auth-db-1 bash -c "sed -i 's/\r$//' pg-restore-auth.sh && chmod +x pg-restore-auth.sh && ./pg-restore-auth.sh"
 endlocal
--- commands/pg-copy-res.bat
+++ commands/pg-copy-res.bat
@@ -3,5 +3,5 @@ setlocal
 set "basedir=%~dp0.."
 set PGPASSWORD=1q2w3e
 pg_dump -U postgres -h localhost -d MicroBiteResources -F c -b -v -f "%basedir%/data/pg-res-data/app/microbiteres.dump"
-docker exec -it microbite-res-db-1 bash -c "chmod +x pg-restore-res.sh && ./pg-restore-res.sh"
+docker exec -it microbite-res-db-1 bash -c "sed -i 's/\r$//' pg-restore-res.sh && chmod +x pg-restore-res.sh && ./pg-restore-res.sh"
 endlocal
